1efacb04e35bfb826aeaa99889885b7e562d268e
bolanebyla <alexleskov2000@gmail.com>
2020-06-16 23:45:44 +0800

    Макет регистрации пользователя

--- bot/views.py
+++ bot/views.py
@@ -1,5 +1,6 @@
 import telebot
 import json
+from telebot import types
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
@@ -7,9 +8,8 @@ from time import sleep
 
 from django.views.decorators.csrf import csrf_exempt
 
-
-
 bot = telebot.TeleBot(settings.TG_TOKEN, threaded=False)
+
 
 @csrf_exempt
 def webhook(request):
@@ -25,7 +25,61 @@ def start_message(message):
                                            '/info - что такое Фудшеринг\n'
                                            '/help - список основных команд\n')
     bot.send_message(chat_id=chat_id,
-                     text='Для того чтобы начать пользоваться моими функциями, пройдите небольшую регистрацию')
+                     text='Для того чтобы начать пользоваться моими функциями, пройдите небольшую регистрацию\n'
+                          'Выберите город',
+                     reply_markup=makeInlineKeyboard_chooseCity())
+
+
+last_data = {}  # Информация о последней нажатой кнопке пользователем
+
+
+# ==================== Обработка Inline кнопок ==================== #
+@bot.callback_query_handler(func=lambda call: True)
+def handle_query(message):
+    global last_data
+    chat_id = message.message.chat.id
+    message_id = message.message.message_id
+    data = message.data
+
+    # Проверка что пользователь не нажал одну и ту же кнопку неколько раз (с одной и той же информацией)
+    if chat_id in last_data.keys() and data == last_data[chat_id]:
+        return
+    last_data[chat_id] = data
+    print(data)
+
+    # После того как пользователь выбрал город
+    if 'city_id' in data:
+        data = json.loads(data)
+
+        # Записываем в БД chat_id пользователя
+        # Записываем в БД город пользователя
+
+        # Выводим сообщение со списком категорий
+        bot.edit_message_text(message_id=message_id, chat_id=chat_id,
+                              text='Выберите категории продуктов о которых вам будут приходить уведомления',
+                              reply_markup=makeInlineKeyboard_chooseCategory())
+
+
+def makeInlineKeyboard_chooseCity(cities=[]):
+    cities = ['Иркутск', 'Москва', 'СПБ']
+    markup = types.InlineKeyboardMarkup()
+    for city in cities:
+        data = json.dumps({"city_id": ''})
+        markup.add(types.InlineKeyboardButton(text=city, callback_data=data))
+    return markup
+
+
+def makeInlineKeyboard_chooseCategory():
+    categories = ['Овощи фрукты, орехи ❌', 'Крупы, макаронные изделия ✅', 'Мясное, рыба ❌', 'Соки, воды ❌',
+                  'Хлебобулочные и кондитерские изделия ✅', 'Консервы ❌', 'Алкоголь ❌', 'Молочная продукция ✅', 'Готовые блюда ✅']
+    markup = types.InlineKeyboardMarkup()
+    for category in categories:
+        data = json.dumps({"category_id": ''})
+        markup.add(types.InlineKeyboardButton(text=category, callback_data=data))
+    markup.add(types.InlineKeyboardButton(text='________________', callback_data='None'))
+    markup.add(types.InlineKeyboardButton(text='➡️ Завершить регистрацию ⬅️', callback_data='end_reg'))
+
+    return markup
 
 
 def start_bot(request):
