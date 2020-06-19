115b29dab4d26f903318b88f5f2649d0b56a8813
bolanebyla <alexleskov2000@gmail.com>
2020-06-19 22:05:59 +0800

    Update urls.py

--- bot/urls.py
+++ bot/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('bot/start', views.start_bot),
-    path('bot/post', views.send_post),
-    path('bot/' + settings.TG_TOKEN, views.webhook),
+    path('start', views.start_bot),
+    path('post', views.send_post),
+    path('' + settings.TG_TOKEN, views.webhook),
 ]
