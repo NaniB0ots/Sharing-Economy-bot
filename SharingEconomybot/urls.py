3eb4417444a64d3c8c04a4b5bef212a2f56be4d6
bolanebyla <alexleskov2000@gmail.com>
2020-06-19 22:06:19 +0800

    Update urls.py

--- SharingEconomybot/urls.py
+++ SharingEconomybot/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bot.urls'))
+    path('bot/', include('bot.urls')),
+    path('vk_parser/', include('vk_parser.urls'))
 ]
